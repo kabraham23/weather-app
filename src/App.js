d4ea83cc64fa9387623263e842c9f6a268b7063a
Katie Abraham <katie.t.abraham@gmail.com>
2021-03-29 17:03:37 -0400

    adding moments usability

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
-import Weather from './components/weather';
+import Weather from './components/Weather'
 
 function App() {
 
@@ -14,8 +14,11 @@ useEffect(() => {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
+
+    console.log("latitude is: ", lat)
+    console.log("longitude is: ", long)
   
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
     .then(res => res.json())
     .then(result => {
       setData(result)
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './styles.css';
 import { Card } from 'semantic-ui-react';
+import moment from 'moment';
 
-const CardExampleCard = ({weatherData}) => (
+const Weather = ({weatherData}) => (
     <Card>
         <Card.Content>
-            <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+            <Card.Header className="header">City Name: {weatherData.name} </Card.Header>
             <p> Temperature: {weatherData.main.temp} </p>
             <p> Sunrise: {weatherData.sys.sunrise} </p>
             <p> Sunset: {weatherData.sys.sunset} </p>
-            <p> Description {weatherData.weather[0].description} </p>
+            <p> Description: {weatherData.weather[0].description} </p>
+            <p> Day: {moment().format('dddd')} </p>
+            <p> Date: {moment().format('LL')} </p>
         </Card.Content>
     </Card>
 )
 
-export default CardExampleCard;
+export default Weather;
+
